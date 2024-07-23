8baa0afaf50ae874f19931b482b2c4e22751cbc4
Arvid Reinert <arvid.reinert@gmail.com>
2024-07-23 09:37:35 +0200

    WIP

---
+++ main.py
@@ -0,0 +1,44 @@
+from rectangle import *
+from setup import *
+class game():
+    def __init__(self):
+        self.running = True
+        self.score = 0
+        self.color_rects = []
+        self.actual_color = self.make_random_base_color()
+        self.color_rect = Rectangle((width/10,height/5),(width/2,height/2),self.actual_color,False)
+
+    def make_random_base_color(self):
+        color = [0,0,0]
+        color[random.choice([0,1,2])] = 250
+        return tuple(color)
+
+    def spawn_random_color_rect(self):
+        color = self.make_random_base_color()
+        color_rect = Rectangle((width/10,height/5),(random.randint(0, width),0),color,False)
+        self.color_rects.append(color_rect)
+
+    def run_game(self):
+        counter = 0
+        while self.running:
+            print(True)
+            clock.tick(30)
+            #spawning rects
+            if counter <= 22:
+                counter += 1
+            if counter == 23:
+                for i in range(1,5):
+                    self.spawn_random_color_rect()
+                counter = 0
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                #here ia the button
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+
+            screen.fill((0,0,0))
+            #rects.update(screen)
+            self.color_rect.update(screen)
+            pygame.display.update()
